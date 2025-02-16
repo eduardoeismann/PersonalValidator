de653930a703041062d8794d8fde8a81ddf1d279
Eduardo Eismann <eduardo.eismann@gmail.com>
2025-02-16 19:41:23 -0300

    StringOutOfBounds Fixed

--- src/validator.java
+++ src/validator.java
@@ -2,12 +2,17 @@
 public class validator {
 	public static void main(String[] args) {
 		Integer v = args[0].hashCode();
-		valid(
-				v.toString().charAt(3),
-				v.toString().charAt(7),
-				v.toString().charAt(5)
-				
-		);
+		
+		try {
+			valid(
+					v.toString().charAt(3),
+					v.toString().charAt(7),
+					v.toString().charAt(5)
+					
+			);
+		} catch(Throwable t) {
+			System.out.println("FALSE");
+		}
 	}
 	
 	private static Boolean valid(char x, char y, char z) {
@@ -15,10 +20,7 @@ public class validator {
 		
 		try {
 			check += x == '7' ? 1 : 0; 
-
 			check += ((int) y - (int) z) == 7 ? 1 : 0;
-			
-			
 		} catch(Throwable t) {
 			System.out.println("FALSE");
 			return false;
